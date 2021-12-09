bf0fe8e7b5b856d5577e4d7e922b4142ee457ad6
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2021-12-09 21:13:01 +0300

    ProductReview and User entities added.

---
+++ untitled/src/main/java/entity/ProductReview.java
@@ -0,0 +1,91 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/* id
+yorum
+yorum tarihi
+urun id
+kullanici id
+ */
+@Entity
+@Table(name = "urunyorum")
+public class ProductReview {
+
+    @SequenceGenerator(name = "generator", sequenceName = "URUNYORUM_ID_SEQ")
+    @Id
+    @GeneratedValue(generator = "generator")
+    private Long id;
+
+    @Column(length = 500, name = "YORUM")
+    private String yorum;
+
+    @Column(name = "YORUM_TARIHI")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date yorumTarihi;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_URUN",
+            foreignKey = @ForeignKey(name = "FK_YORUM_URUN_ID")
+    )
+    private Product urunId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_KULLANICI",
+            foreignKey = @ForeignKey(name = "FK_YORUM_KULLANICI_ID")
+    )
+    private User kullaniciId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getYorum() {
+        return yorum;
+    }
+
+    public void setYorum(String yorum) {
+        this.yorum = yorum;
+    }
+
+    public Date getYorumTarihi() {
+        return yorumTarihi;
+    }
+
+    public void setYorumTarihi(Date yorumTarihi) {
+        this.yorumTarihi = yorumTarihi;
+    }
+
+    public Product getUrunId() {
+        return urunId;
+    }
+
+    public void setUrunId(Product urunId) {
+        this.urunId = urunId;
+    }
+
+    public User getKullaniciId() {
+        return kullaniciId;
+    }
+
+    public void setKullaniciId(User kullaniciId) {
+        this.kullaniciId = kullaniciId;
+    }
+
+    @Override
+    public String toString() {
+        return "UrunYorum{" +
+                "id=" + id +
+                ", yorum='" + yorum + '\'' +
+                ", yorumTarihi=" + yorumTarihi +
+                ", urunId=" + urunId +
+                ", kullaniciId=" + kullaniciId +
+                '}';
+    }
+
+}
---
+++ untitled/src/main/java/entity/User.java
@@ -0,0 +1,77 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "kullanici")
+public class User {
+
+    @SequenceGenerator(name = "generator", sequenceName = "KULLANICI_ID_SEQ")
+    @Id
+    @GeneratedValue(generator = "generator")
+    private Long id;
+
+    @Column(length = 50, name = "ADI")
+    private String adi;
+
+    @Column(length = 50, name = "SOYADI")
+    private String soyadi;
+
+    @Column(length = 50, name = "EMAIL")
+    private String email;
+
+    @Column(length = 15, name = "TELEFON")
+    private String telefon;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public String getSoyadi() {
+        return soyadi;
+    }
+
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    @Override
+    public String toString() {
+        return "Kullanici{" +
+                "id=" + id +
+                ", adi='" + adi + '\'' +
+                ", soyadi=" + soyadi +
+                ", email=" + email +
+                ", telefon=" + telefon +
+                '}';
+    }
+
+}
